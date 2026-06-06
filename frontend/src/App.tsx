a9efaf5d3c864c2a55c9738a589ad9b53be6f229
Mayank Acharya <mayank.acharya.official@gmail.com>
2026-06-06 14:07:19 +0530

    Added first 3D star field

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,44 @@
+import SpaceScene from "./components/SpaceScene";
+
 function App() {
   return (
     <div
       style={{
         width: "100vw",
         height: "100vh",
-        background: "#000",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        color: "white",
+        position: "relative",
+        background: "black",
       }}
     >
-      <h1
-        style={{
-          fontSize: "4rem",
-          marginBottom: "20px",
-        }}
-      >
-        ACHARYAVERSE
-      </h1>
+      <SpaceScene />
 
-      <p
+      <div
         style={{
-          fontSize: "1.2rem",
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          color: "white",
+          textAlign: "center",
         }}
       >
-        Exploring Software, AI and Physics
-      </p>
+        <h1
+          style={{
+            fontSize: "4rem",
+            marginBottom: "15px",
+          }}
+        >
+          ACHARYAVERSE
+        </h1>
+
+        <p
+          style={{
+            fontSize: "1.2rem",
+          }}
+        >
+          Exploring Software, AI and Physics
+        </p>
+      </div>
     </div>
   );
 }
---
+++ frontend/src/components/SpaceScene.tsx
@@ -0,0 +1,20 @@
+import { Canvas } from "@react-three/fiber";
+import { Stars } from "@react-three/drei";
+
+export default function SpaceScene() {
+  return (
+    <Canvas camera={{ position: [0, 0, 5] }}>
+      <ambientLight intensity={0.5} />
+
+      <Stars
+        radius={100}
+        depth={50}
+        count={5000}
+        factor={4}
+        saturation={0}
+        fade
+        speed={1}
+      />
+    </Canvas>
+  );
+}
